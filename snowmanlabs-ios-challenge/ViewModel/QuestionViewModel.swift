8964859b2e2a976dda06f07ec3c4f9ec21fb9f17
José Matela <jmatelaneto@gmail.com>
2021-02-15 14:58:27 -0300

    finalizing question viewmodel

--- snowmanlabs-ios-challenge/ViewModel/QuestionViewModel.swift
+++ snowmanlabs-ios-challenge/ViewModel/QuestionViewModel.swift
@@ -6,3 +6,42 @@
 //
 
 import Foundation
+
+protocol QuestionViewModelDelegate {
+    func getQuestions()
+    func addedNewQuestion()
+}
+
+class QuestionViewModel {
+    
+    var questions: [Question] = []
+    var service = QuestionService()
+    var delegate: QuestionViewModelDelegate!
+    
+    func getQuestions() {
+        self.service.delegate = self
+        self.service.observeQuestions()
+    }
+    
+    func postQuestion(title: String, answer: String, color: Int) {
+        self.service.delegate = self
+        self.service.postQuestion(title: title, answer: answer, color: color)
+    }
+    
+    func updateExpandedCellValue(question: Question, index: Int) {
+        self.questions[index].expanded = !question.expanded
+    }
+}
+
+extension QuestionViewModel: QuestionsServiceDelegate {
+    func fetchQuestions(questions: [Question]) {
+        self.questions = questions
+        self.delegate.getQuestions()
+    }
+    
+    func createQuestion(question: Question) {
+        self.questions.append(question)
+        
+        self.delegate.addedNewQuestion()
+    }
+}
