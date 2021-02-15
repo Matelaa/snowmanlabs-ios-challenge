f472df053d4788a3c78dac80526cf918af94b7a9
José Matela <jmatelaneto@gmail.com>
2021-02-15 13:39:28 -0300

    creating a tableview to list questions

--- snowmanlabs-ios-challenge/Controllers/FAQ Home/FAQHomeViewController.swift
+++ snowmanlabs-ios-challenge/Controllers/FAQ Home/FAQHomeViewController.swift
@@ -9,11 +9,41 @@ import UIKit
 
 class FAQHomeViewController: UIViewController {
 
+    lazy var tableView: UITableView = {
+        let tableView = UITableView(frame: .zero, style: .grouped)
+        
+        tableView.translatesAutoresizingMaskIntoConstraints = false
+        tableView.backgroundColor = .red
+        
+        self.view.addSubview(tableView)
+        
+        return tableView
+    }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
         self.title = "Perguntas Frequentes"
         
+        self.setupUI()
+    }
+    
+    private func setupUI() {
         self.view.backgroundColor = .white
+        
+        self.setupTableView()
+    }
+    
+    private func setupTableView() {
+        self.setupTableViewConstraints()
+    }
+    
+    private func setupTableViewConstraints() {
+        NSLayoutConstraint.activate([
+            self.tableView.topAnchor.constraint(equalTo: self.view.topAnchor),
+            self.tableView.leftAnchor.constraint(equalTo: self.view.leftAnchor, constant: 8),
+            self.tableView.rightAnchor.constraint(equalTo: self.view.rightAnchor, constant: -8),
+            self.tableView.bottomAnchor.constraint(equalTo: self.view.bottomAnchor, constant: -24),
+        ])
     }
 }
