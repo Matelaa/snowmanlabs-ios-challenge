e4c08763f4676933488b0429c0756614748e613a
José Matela <jmatelaneto@gmail.com>
2021-02-15 14:39:47 -0300

    finalizing question service

--- snowmanlabs-ios-challenge/Service/QuestionService.swift
+++ snowmanlabs-ios-challenge/Service/QuestionService.swift
@@ -6,3 +6,54 @@
 //
 
 import Foundation
+import FirebaseDatabase
+
+protocol QuestionsServiceDelegate {
+    func fetchQuestions(questions: [Question])
+    func createQuestion(question: Question)
+}
+
+class QuestionService {
+    
+    let questionRef = Database.database().reference().child("questions")
+    
+    var delegate: QuestionsServiceDelegate!
+    
+    func observeQuestions() {
+        
+        self.questionRef.observe(.value) { snapshot in
+            
+            var questions = [Question]()
+            
+            for child in snapshot.children {
+                if let childSnapshot = child as? DataSnapshot,
+                   let dict = childSnapshot.value as? [String:Any],
+                   let title = dict["title"] as? String,
+                   let answer = dict["answer"] as? String,
+                   let color = dict["color"] as? Int {
+                    
+                    let question = Question(title: title, answer: answer, color: color, expanded: false)
+                    
+                    questions.append(question)
+                }
+            }
+            
+            self.delegate.fetchQuestions(questions: questions)
+        }
+    }
+    
+    func postQuestion(title: String, answer: String, color: Int) {
+        
+        let questionRef = self.questionRef.childByAutoId()
+        
+        let postQuestion = Question(title: title, answer: answer, color: color, expanded: false)
+        
+        questionRef.setValue([
+            "title": postQuestion.title,
+            "answer": postQuestion.answer,
+            "color": postQuestion.color,
+        ])
+        
+        self.delegate.createQuestion(question: postQuestion)
+    }
+}
