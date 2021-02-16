9a069a8ecc2aece9f082a33e044f35c1363b7f01
José Matela <jmatelaneto@gmail.com>
2021-02-16 12:56:59 -0300

    creating new controller to create new questions

---
+++ snowmanlabs-ios-challenge/Controllers/FAQ Create/FAQNewQuestionViewController.swift
@@ -0,0 +1,17 @@
+//
+//  FAQNewQuestionViewController.swift
+//  snowmanlabs-ios-challenge
+//
+//  Created by José Matela Neto on 16/02/21.
+//
+
+import UIKit
+
+class FAQNewQuestionViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        self.view.backgroundColor = .purple
+    }
+}
